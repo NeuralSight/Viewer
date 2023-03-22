549699cc8e8875aa2bba996c737c8cba4acf8fdd
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-03-22 23:03:44 +0300

    Create Select.tsx

---
+++ neuralsight-extensions/neuralsight-tools/src/components/select-ai-model/Select.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ButtonGroup } from '@ohif/ui';
+
+type Props = {};
+
+const Select = (props: Props) => {
+  return <div>Select</div>;
+};
+
+export default Select;
