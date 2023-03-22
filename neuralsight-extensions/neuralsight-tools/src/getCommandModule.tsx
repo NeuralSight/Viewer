4728cfe70647574b16dd3a391cd1b4bb36d75905
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-03-22 23:02:31 +0300

    Create getCommandModule.tsx
    list ALL command that will be available

---
+++ neuralsight-extensions/neuralsight-tools/src/getCommandModule.tsx
@@ -0,0 +1,76 @@
+import { ServicesManager } from '@ohif/core';
+import NeuralSightViewportUploadForm from './forms/NeuralSightViewportUploadForm';
+
+/**
+ * CommandsModule should provide a list of commands that will be available in OHIF
+ * for Modes to consume and use in the viewports. Each command is defined by
+ * an object of { actions, definitions, defaultContext } where actions is an
+ * object of functions, definitions is an object of available commands, their
+ * options, and defaultContext is the default context for the command to run against.
+ */
+
+function getCommandsModule({
+  servicesManager,
+  commandsManager,
+  extensionManager,
+}) {
+  const {
+    viewportGridService,
+    toolGroupService,
+    cineService,
+    toolbarService,
+    uiDialogService,
+    cornerstoneViewportService,
+    hangingProtocolService,
+    uiNotificationService,
+    uiModalService,
+  } = (servicesManager as ServicesManager).services;
+
+  return {
+    definitions: {
+      showUploadViewportModal: {
+        commandFn: () => {
+          // viewportGridService gets the active ViewPort
+          const { activeViewportIndex } = viewportGridService.getState();
+          //
+
+          if (uiModalService) {
+            uiModalService.show({
+              title: 'Upload A CT Image for AI probing',
+              content: NeuralSightViewportUploadForm,
+              contentProps: {
+                activeViewportIndex,
+                onClose: uiModalService.hide,
+                cornerstoneViewportService,
+              },
+            });
+          }
+        },
+        options: {
+          // command options comes
+        },
+        context: 'NEURALSIGHT', // optional
+      },
+    },
+    // defaultContext: 'ACTIVE_VIEWPORT::DICOMSR',
+  };
+}
+// showUploadViewportModal: () => {
+//   // viewportGridService gets the active ViewPort
+//   const { activeViewportIndex } = viewportGridService.getState();
+//   //
+//   const { uiModalService } = servicesManager.services;
+//   if (uiModalService) {
+//     uiModalService.show({
+//       title: 'Upload A CT Image for AI probing',
+//       content: NeuralSightViewportUploadForm,
+//       contentProps: {
+//         activeViewportIndex,
+//         onClose: uiModalService.hide,
+//         cornerstoneViewportService,
+//       },
+//     });
+//   }
+// }
+
+export default getCommandsModule;
