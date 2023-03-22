6290846bafff57fa10ca24970b053fb48f3e39e9
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-03-22 23:01:21 +0300

    Create getContextModule.tsx
    context module

---
+++ neuralsight-extensions/neuralsight-tools/src/getContextModule.tsx
@@ -0,0 +1,7 @@
+/**
+ * TODO:
+ * ContextModule should provide a list of context that will be available in OHIF
+ * and will be provided to the Modes. A context is a state that is shared OHIF.
+ * Context is defined by an object of { name, context, provider }. Examples include
+ * the measurementTracking context provided by the measurementTracking extension.
+ */
