8c6a3b216a7868bb9ebd8e5210dff42bc68847e8
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-03-22 22:56:14 +0300

    AI settings component

---
+++ neuralsight-extensions/neuralsight-tools/src/panel/AiSettings/AISettings.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Title from '../../components/ai-panel-title';
+type Props = {};
+
+const AISettings = (props: Props) => {
+  return (
+    <div className="text-white">
+      <Title title="settings" />
+      <div className="gap-2"></div>
+    </div>
+  );
+};
+
+export default AISettings;
---
+++ neuralsight-extensions/neuralsight-tools/src/panel/AiSettings/index.ts
@@ -0,0 +1,3 @@
+import AISettings from './AISettings';
+// Data will come through here passed to the Ai report before being exported
+export default AISettings;
