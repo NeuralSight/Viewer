2c69e2f1df6bd5a85fac71e3fd01e51f3cd0dbbe
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-03-22 23:04:56 +0300

    panel title and it's story file

---
+++ neuralsight-extensions/neuralsight-tools/src/components/ai-panel-title/Title.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+type Props = {
+  title: string;
+};
+
+const Title = ({ title }: Props) => {
+  const { t } = useTranslation('Titles');
+  return (
+    <div className="flex justify-between px-2 py-1 bg-secondary-main">
+      <span className="text-base font-bold tracking-widest text-white uppercase">
+        {t(title)}
+      </span>
+    </div>
+  );
+};
+
+export default Title;
