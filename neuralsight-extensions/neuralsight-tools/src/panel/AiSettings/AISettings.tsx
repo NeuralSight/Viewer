b4d28a60fbac655f60e9be6c4da606c655c514d9
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-03-24 15:12:33 +0300

    Update AISettings.tsx

--- neuralsight-extensions/neuralsight-tools/src/panel/AiSettings/AISettings.tsx
+++ neuralsight-extensions/neuralsight-tools/src/panel/AiSettings/AISettings.tsx
@@ -1,49 +1,80 @@
-import React, { useState, useId } from 'react';
+import React, { useState, useEffect } from 'react';
 import Title from '../../components/ai-panel-title';
 import Select from '../../components/select-ai-model';
 import data from '../../utils/model-test-data.json';
-import { SelectType, UniqueModelInfoDataType } from '../../../data';
+import { SelectType } from '../../../data';
+import ListItem from '../../components/item-listing';
 
 import { setStorageItem, getStorageItem } from '../../utils/localStorageAccess';
 
 const SELECTED_MODEL = 'selected_model';
 
+//TODO: alternative use a localstorage with expiry
 type Props = {};
 const AISettings = (props: Props) => {
   const id = 'model';
-  let modelNames: SelectType[] = [];
+  let modelValues: SelectType[] = [];
+
   for (let i = 0; i < data.length; i++) {
     let modelObj: SelectType = {
       value: data[i].model_name,
       label: data[i].model_name,
     };
-    modelNames = [...modelNames, modelObj];
+    modelValues = [...modelValues, modelObj];
   }
-  const currentModel = modelNames.find(
-    model => getStorageItem(SELECTED_MODEL).model_name === model.label
-  );
-  console.log('currentModel', currentModel);
 
-  console.log('modelsName', modelNames);
-  const [modelName, setModelName] = useState<SelectType | null>(modelNames[0]);
+  // console.log('currentModel', currentModel);
+  // console.log('modelsName', modelValues);
+  const [modelValue, setModelValue] = useState<SelectType | null>(null);
+
   const handleModelSelected = (value: SelectType) => {
     console.log('value', value);
-    setModelName(value);
-    setStorageItem(
-      SELECTED_MODEL,
-      data.find(item => item.model_name === value.label)
-    );
+    setModelValue(value);
+    setStorageItem(SELECTED_MODEL, value);
   };
+
+  useEffect(() => {
+    let currentData: SelectType = getStorageItem(SELECTED_MODEL);
+    setModelValue(() => currentData);
+  }, [setModelValue]);
+  const currentModel = data.find(
+    model => model.model_name === modelValue?.label
+  );
+  const keys = Object.keys(currentModel || {});
+
   return (
     <div className="text-white">
-      <Title title="settings" />
-      <div className="w-full mt-4">
+      <Title title="model specifications" />
+      <div className="w-full mt-4 space-y-3">
         <Select
-          selectData={modelNames}
-          value={modelName}
+          selectData={modelValues}
+          value={modelValue}
           id={id}
           onChange={handleModelSelected}
+          labelText={'Available AI Models'}
         />
+        <div>
+          {modelValue && currentModel ? (
+            <table
+              className="space-y-1 table-auto"
+              aria-label="model-specifications"
+            >
+              <tbody className="">
+                {keys.map((key, index) => (
+                  <ListItem
+                    key={key + index}
+                    label={key.replace(/_/g, ' ')}
+                    value={currentModel[key]?.replace(/_/g, ' ')}
+                  />
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <p className=" w-full text-lg capitalize text-blue-300">
+              select a model
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
