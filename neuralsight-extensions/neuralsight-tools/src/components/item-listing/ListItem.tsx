f20af3f9eb3b2de2797a9a0f2f4d366efe81cbb9
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-03-27 11:12:33 +0300

    Update ListItem.tsx

--- neuralsight-extensions/neuralsight-tools/src/components/item-listing/ListItem.tsx
+++ neuralsight-extensions/neuralsight-tools/src/components/item-listing/ListItem.tsx
@@ -3,15 +3,41 @@ import React from 'react';
 type Props = {
   value: string;
   label: string;
+  paddingX:
+    | 'px-0'
+    | 'px-0.5'
+    | 'px-1'
+    | 'px-1.5'
+    | 'px-2'
+    | 'px-3'
+    | 'px-4'
+    | 'px-5'
+    | 'px-6';
+
+  paddingY:
+    | 'py-0'
+    | 'py-0.5'
+    | 'py-1'
+    | 'py-1.5'
+    | 'py-2'
+    | 'py-3'
+    | 'py-4'
+    | 'py-5'
+    | 'py-6';
 };
 
-const ListItem = ({ value, label }: Props) => {
+const ListItem = ({ value, label, paddingX, paddingY }: Props) => {
   return (
-    <tr className="">
-      <td className="text-blue-300  text-xs font-medium whitespace-normal">
+    <tr className="border border-secondary-light">
+      <td
+        className={`text-blue-300 text-xs font-medium whitespace-normal ${paddingX} ${paddingY}`}
+        style={{ verticalAlign: 'top' }}
+      >
         {label}
       </td>
-      <td className="text-common-bright text-xs">{value}</td>
+      <td className={`text-common-bright text-xs  ${paddingX} ${paddingY}`}>
+        {value}
+      </td>
     </tr>
   );
 };
