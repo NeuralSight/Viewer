60f454621df712ac8f4920593fab2e694deeb49d
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-03-27 11:12:20 +0300

    Update AIReport.tsx

--- neuralsight-extensions/neuralsight-tools/src/panel/AiReport/AIReport.tsx
+++ neuralsight-extensions/neuralsight-tools/src/panel/AiReport/AIReport.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Title from '../../components/ai-panel-title';
+import ListItem from '../../components/item-listing/ListItem';
+import testData from '../../utils/pathogen.json';
 
 type Props = {
   commandsManager: object;
@@ -10,7 +12,37 @@ type Props = {
 const AIReport = (props: Props): React.ReactNode => {
   return (
     <div className="text-white">
-      <Title title="ai Findings" />
+      <Title title="AI Findings" />
+      <div className="w-full mt-4 space-y-3">
+        {testData ? (
+          <table
+            className="border-collapse border-spacing-y-4 border-spacing-x-1 border border-secondary-light table-fixed w-full"
+            aria-label="model-specifications"
+          >
+            <thead className="capitalize font-bold text-sm text-">
+              <tr className="border border-secondary-light ">
+                <td className="p-3">pathogens</td>
+                <td className="p-3">percentage</td>
+              </tr>
+            </thead>
+            <tbody>
+              {testData.map((data, index) => (
+                <ListItem
+                  key={index}
+                  label={Object.keys(data)[0]}
+                  value={`${data[Object.keys(data)[0]]?.percentage}%` || ''}
+                  paddingX="px-3"
+                  paddingY="py-3"
+                />
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p className=" w-full text-lg capitalize text-blue-300">
+            select a model
+          </p>
+        )}
+      </div>
     </div>
   );
 };
