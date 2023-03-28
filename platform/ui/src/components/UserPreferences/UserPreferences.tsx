ef977df855539bad02cf8564bb57f534b41afb69
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-03-28 16:29:12 +0300

    Update UserPreferences.tsx

--- platform/ui/src/components/UserPreferences/UserPreferences.tsx
+++ platform/ui/src/components/UserPreferences/UserPreferences.tsx
@@ -85,6 +85,7 @@ const UserPreferences = ({
             onChange={onLanguageChangeHandler}
             options={availableLanguages}
             value={state.language}
+            closeMenuOnSelect
           />
         </div>
       </Section>
