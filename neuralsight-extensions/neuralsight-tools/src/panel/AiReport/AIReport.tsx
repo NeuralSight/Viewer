d1abd19b9f951f5cc2cd267d4000948e8da2a180
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-07-11 18:04:29 +0300

    Ai report comsuming from backend

--- neuralsight-extensions/neuralsight-tools/src/panel/AiReport/AIReport.tsx
+++ neuralsight-extensions/neuralsight-tools/src/panel/AiReport/AIReport.tsx
@@ -1,20 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Title from '../../components/ai-panel-title';
 import ListItem from '../../components/item-listing/ListItem';
-import testData from '../../utils/pathogen.json';
+// import testData from '../../utils/pathogen.json'; TODO: REMOVE THIS AND TEST DATAS
+import { useTranslation } from 'react-i18next';
+import { CommandsManager, ExtensionManager, ServicesManager } from '@ohif/core';
+import { getAIPredResultForStudy } from '../../utils/api';
+import { AiResultError, AiResultType } from '../../../data';
+import { Typography } from '@ohif/ui';
+import { formatValueToPercentage } from '../../utils/FloatToPercentageString';
 
 type Props = {
-  commandsManager: object;
-  extensionManager: object;
-  servicesManager: object;
+  commandsManager: CommandsManager;
+  extensionManager: ExtensionManager;
+  servicesManager: ServicesManager;
 };
 
 const AIReport = (props: Props): React.ReactNode => {
+  const { t } = useTranslation();
+
+  // FIXME: use window object to get StudyInstanceUIDs but update and use ohig internal getter method if any instead incase the start using ssr
+  const urlArr = document.baseURI.split('?');
+  const URL = urlArr[0]; //Base Url
+  const StudyInstanceUIDs = urlArr[1].split('=')[1]; //StudyInstanceUIDs
+  const [id, setID] = useState(StudyInstanceUIDs);
+  const [AIresult, setAIResult] = useState<AiResultType>();
+  const [error, setError] = useState<AiResultError>();
+
+  // do a get request here
+  const handleGetResult = async () => {
+    try {
+      const data = await getAIPredResultForStudy({ uuid: id });
+      setAIResult(data);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  console.log('Airesult', AIresult?.results);
+  console.log('error', error);
+  useEffect(() => {
+    handleGetResult();
+  }, []);
+
   return (
     <div className="text-white">
       <Title title="AI Findings" />
       <div className="w-full mt-4 space-y-3">
-        {testData ? (
+        {error && (
+          <Typography className="pl-1 my-2" color="error">
+            {t(`ErrorMessage: ${error.detail}`)}
+          </Typography>
+        )}
+        {AIresult ? (
           <table
             className="border-collapse border-spacing-y-4 border-spacing-x-1 border border-secondary-light table-fixed w-full"
             aria-label="model-specifications"
@@ -26,20 +63,34 @@ const AIReport = (props: Props): React.ReactNode => {
               </tr>
             </thead>
             <tbody>
-              {testData.map((data, index) => (
-                <ListItem
-                  key={index}
-                  label={Object.keys(data)[0]}
-                  value={`${data[Object.keys(data)[0]]?.percentage}%` || ''}
-                  paddingX="px-3"
-                  paddingY="py-3"
-                />
-              ))}
+              {AIresult?.results.preds &&
+              Object.values(AIresult.results.preds).length > 0 ? (
+                Object.keys(AIresult.results.preds).map((data, index) => (
+                  <ListItem
+                    key={data + index}
+                    label={t(data)}
+                    value={formatValueToPercentage(
+                      AIresult.results.preds[data]
+                    )}
+                    paddingX="px-3"
+                    paddingY="py-3"
+                  />
+                ))
+              ) : (
+                <tr className="border border-secondary-light">
+                  <td
+                    colSpan={2}
+                    className="text-blue-300 text-sm font-medium whitespace-normal px-3 py-3"
+                  >
+                    {t('No problem incounter in this Study look okay 👍')}
+                  </td>
+                </tr>
+              )}
             </tbody>
           </table>
         ) : (
           <p className=" w-full text-lg capitalize text-blue-300">
-            select a model
+            {t('select a model and upload a image!')}
           </p>
         )}
       </div>
--- neuralsight-extensions/neuralsight-tools/src/utils/api.ts
+++ neuralsight-extensions/neuralsight-tools/src/utils/api.ts
@@ -1,13 +1,19 @@
-import { AnyObject, PostImageType } from '../../data';
+import {
+  AiResultError,
+  AiResultType,
+  AnyObject,
+  PostImageType,
+} from '../../data';
 import {
   changeObjToFormData,
   changeObjToFormUrlencoded,
 } from './changeObjToFormData';
 
-// trying with orthanc
+//if not specified use the defaults;
+const NeuralSightBackend = `${process.env.REACT_APP_API_URL}/api/v1/patient/dicom`;
+const Dicom = process.env.REACT_APP_ORTHANC_URL;
 
-const NeuralSightBackend = '/api/v1/patient/dicom'; //TODO: should be in an enironment
-const Dicom = `https://orthanc.neuralsight.ai/studies/`;
+//TOFIX: remove this when not need again
 const username = 'asdas';
 const password = 'asdasd';
 
@@ -21,31 +27,43 @@ const password = 'asdasd';
 //   Host: 'localhost:8042',
 // });
 
-// post and predict a patient dicom images
 export const postPatientStudy = async ({
   patientID,
   file,
 }: PostImageType): Promise<AnyObject> => {
-  const response = await fetch(
-    `https://backend.neuralsight.ai${NeuralSightBackend}/pred  `,
-    {
-      method: 'POST',
-      body: changeObjToFormData({
-        file,
-        username,
-        password,
-        file_refence: patientID,
-      }),
-    }
-  );
+  const response = await fetch(`${NeuralSightBackend}/pred  `, {
+    method: 'POST',
+    body: changeObjToFormData({
+      file,
+      username,
+      password,
+      file_refence: patientID,
+    }),
+  });
   return response;
 };
 
-// get studies info
 export const getStudyInfoFromImageId = async (
   id: string
 ): Promise<AnyObject> => {
   console.log('DICOMWeb', Dicom + id);
-  const response = await fetch(`${Dicom}${id}`);
+  const response = await fetch(`${Dicom}/studies/${id}`);
   return response;
+};
+
+export const getAIPredResultForStudy = async ({
+  uuid,
+}: {
+  uuid: string;
+}): Promise<AiResultType> => {
+  const response = await fetch(
+    `${NeuralSightBackend}/{dicom_uuid}?uuid=${uuid}`
+  );
+  const data = await response.json();
+  if (response.status === 200 || response.status === 201) {
+    return data as AiResultType;
+  } else {
+    const error = data as AiResultError;
+    throw error;
+  }
 };
